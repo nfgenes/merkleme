a21bafcc8529954a3071aec9788304c38d39b31f
Orlundo Hubbard <orlundokhubbard@gmail.com>
2022-02-28 10:15:46 -0600

    feat: begin landing page styles

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,8 +5,16 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.home}>
+    <div>
+    <nav className={styles.navBar}>
+      <Image src={landingPageData.treeImg} width="76px" height="69px" className={styles.tree} /> 
       <h1 className={styles.title}>{landingPageData.title}</h1>
+    </nav>
+
+    {/* Beginning of hero section */}
+    <div className={styles.heroContainer}>
+      <h1 className={styles.heroHeading}>{landingPageData.heroHeading}</h1>
+    </div>
     </div>
   );
 }
