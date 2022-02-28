a9bd503a0840c34116b12b767a2152cfc7f202ae
Yusuf Kehinde Hussein <yusufkehinde11@gmail.com>
2022-02-28 11:40:03 +0100

    feat: added data and some css variables

---
+++ data/landingpage.js
@@ -0,0 +1,8 @@
+export const landingPageData = {
+  treeImg: "/assets/tree.png",
+  title: "MerkleMe",
+  heroHeading: "Merkle Trees the easy way!",
+  heroSubheading: "Generate. Publish. Verify.",
+  ctaBtn: "Get Started ",
+  handcraftImg: "/assets/handcraft.png",
+};
