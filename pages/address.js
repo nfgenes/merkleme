1134dce605a2de6f2dddda949200f317ca9e9093
Yusuf Kehinde Hussein <yusufkehinde11@gmail.com>
2022-03-02 16:04:08 +0100

    feat: added address page

--- pages/address.js
+++ pages/address.js
@@ -3,17 +3,39 @@ import Image from "next/image";
 import styles from "../styles/address.module.css";
 
 export default function Address() {
-    
-    return (
-        <div>
-            <nav className={styles.addressNav}>
-                <div className={ styles.treeImgContainer}>
-                <Image src={addressPage.treeImgAsset} width="50px" height="69px" alt="treeImg" className={styles.treeImgAsset} />
-                </div>
-                <div className={styles.title}>
-                    {addressPage.merkleTitle}
-                </div>
-            </nav>
+  return (
+    <main className={styles.main}>
+      <nav className={styles.nav}>
+        <div className={styles.treeImgContain}>
+          <Image
+            src={addressPage.treeImgAsset}
+            width="50px"
+            height="69px"
+            alt="treeImg"
+            className={styles.treeImg}
+          />
         </div>
-    )
+        <div className={styles.title}>{addressPage.merkleTitle}</div>
+      </nav>
+
+      <section className={styles.addressSection}>
+        <div className={styles.heading}>{addressPage.addressHeading}</div>
+
+        <div className={styles.addressFormSection}>
+          <textarea
+            placeholder={addressPage.placeholderText}
+            rows="20"
+            cols="40"
+            className={styles.textarea}
+          ></textarea>
+
+          <div className={styles.buttonWrapper}>
+            <button className={styles.nextStyles}>
+              {addressPage.merkleButton}
+            </button>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
