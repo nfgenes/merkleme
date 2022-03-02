b3f01278ef2346a2ff1ede6386e8920cf9649f14
Orlundo Hubbard <orlundokhubbard@gmail.com>
2022-03-01 22:58:38 -0600

    feat: add form pages

---
+++ data/information.js
@@ -0,0 +1,8 @@
+export const informationData = {
+    treeImg: "/assets/tree.png",
+    title: "MerkleMe",
+    heading: "We'll just need a couple of things first!",
+    collectionTitle: "Collection name (required)",
+    emailTitle: "Email (optional)",
+    nextButton: "Submit",
+};
